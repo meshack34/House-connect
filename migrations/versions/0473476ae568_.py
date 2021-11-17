50ad56b58442dbb95afb330a9cb5273406356188
asandelarvine <asandelarvine@gmail.com>
2021-11-17 16:37:32 +0300

    added

---
+++ migrations/versions/0473476ae568_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 0473476ae568
+Revises: ddbce3aabcde
+Create Date: 2021-11-16 22:29:02.444974
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0473476ae568'
+down_revision = 'ddbce3aabcde'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/1d0880d61334_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 1d0880d61334
+Revises: 0473476ae568
+Create Date: 2021-11-16 22:40:58.920143
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1d0880d61334'
+down_revision = '0473476ae568'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
